5084430a6c3cb5de3435789c25de28d741ac434a
Christopher Jones <cjones999983@gmail.com>
2018-06-10 15:06:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,9 +43,7 @@ class Student
     SQL
 
     student_row = DB[:conn].execute(sql, name)[0]
-    student = self.new_from_db(student_row)
-    student
-    binding.pry
+    self.new_from_db(student_row)
   end
 
   def save
